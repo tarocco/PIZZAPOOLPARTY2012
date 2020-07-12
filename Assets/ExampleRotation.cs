b723579e5a653fa0a47eb33da969806e86cf819c
Tarocco <bun@taroc.co>
2020-07-11 22:10:53 -0700

    1.uhoh

---
+++ Assets/ExampleRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ExampleRotation : MonoBehaviour, ISerializationCallbackReceiver
+{
+    [SerializeField, HideInInspector]
+    private Rigidbody _Rigidbody;
+
+    public Rigidbody Rigidbody
+    {
+        get { return _Rigidbody; }
+        set { _Rigidbody = value; }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var parent_rotation = transform.parent?.rotation ?? Quaternion.identity;
+        var point = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        point = point - new Vector3(0.5f, 0.5f, 0f);
+        var rotation = Quaternion.Euler(180f * point.y, -180f * point.x, 0f);
+        Rigidbody.MoveRotation(rotation * parent_rotation);
+    }
+
+    public void OnBeforeSerialize()
+    {
+        if (Rigidbody == null)
+            Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void OnAfterDeserialize()
+    {
+    }
+}
---
+++ Assets/VirtualParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualParent : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _VirtualParentTransform;
+    public Transform VirtualParentTransform
+    {
+        get { return _VirtualParentTransform; }
+        set { _VirtualParentTransform = value; }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = VirtualParentTransform.position;
+        transform.rotation = VirtualParentTransform.rotation;
+        transform.localScale = VirtualParentTransform.localScale;
+    }
+}
