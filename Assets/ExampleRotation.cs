236567ec16c7509fba307c06f2f3020ab25eb397
Tarocco <bun@taroc.co>
2020-07-12 14:31:24 -0700

    interactive pizza 2

--- Assets/ExampleRotation.cs
+++ Assets/ExampleRotation.cs
@@ -14,9 +14,19 @@ public class ExampleRotation : MonoBehaviour, ISerializationCallbackReceiver
         set { _Rigidbody = value; }
     }
 
+    [SerializeField, Range(0f, 2f)]
+    private float _Sensitivity = 1f;
+
+    public float Sensitivity
+    {
+        get { return _Sensitivity; }
+        set { _Sensitivity = value; }
+    }
+
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -24,6 +34,10 @@ public class ExampleRotation : MonoBehaviour, ISerializationCallbackReceiver
         var parent_rotation = transform.parent?.rotation ?? Quaternion.identity;
         var point = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         point = point - new Vector3(0.5f, 0.5f, 0f);
+        point = Sensitivity * point;
+        //var rotation = Quaternion.Euler(-180f * point.x, 0f, -180f * point.y);
+        //Rigidbody.MoveRotation(parent_rotation * rotation);
+
         var rotation = Quaternion.Euler(180f * point.y, -180f * point.x, 0f);
         Rigidbody.MoveRotation(rotation * parent_rotation);
     }
